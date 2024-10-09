690b631b1b8569768f47d88f93a81e4bc8d6f1e6
anisanhy <joyabrianrantepasang14@gmail.com>
2024-10-09 13:35:22 +0800

    video 4 Group & Dynamic Routes

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public List<Camera> cameras;
+    private int currentCameraIndex = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < cameras.Count; i++)
+        {
+            cameras[i].gameObject.SetActive(i == currentCameraIndex);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchCamera();
+        }
+    }
+
+    void SwitchCamera()
+    {
+        cameras[currentCameraIndex].gameObject.SetActive(false);
+
+        // Move to the next camera
+        currentCameraIndex = (currentCameraIndex + 1) % cameras.Count;
+
+        // Activate the new current camera
+        cameras[currentCameraIndex].gameObject.SetActive(true);
+    }
+}
