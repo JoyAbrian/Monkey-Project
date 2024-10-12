4e68a8a220c6fbc7b8f10fbceced8850b394ef68
anisanhy <joyabrianrantepasang14@gmail.com>
2024-10-12 22:09:01 +0800

    adding sound & sound manager

---
+++ Assets/Custom Script/SoundManager/SoundValues.cs
@@ -0,0 +1,21 @@
+//Author: Small Hedge Games
+//Updated: 13/06/2024
+
+namespace SmallHedge.SoundManager
+{
+    public enum SoundType
+    {
+        Menu,
+        EndingWin,
+        EndingLose,
+        Anomaly1,
+        Anomaly2,
+        Anomaly3,
+        AmbienceStairs,
+        AmbiencePatientRoom,
+        AmbienceHallway,
+        AmbienceHall,
+        AmbienceControlRoom,
+        AmbienceCanteen
+    }
+}
